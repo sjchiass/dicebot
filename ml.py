4378100b50bd24401b54518c040662b70f8cedb2
Sebastien Chiasson <sjchiass@gmail.com>
2022-11-12 15:33:22 -0500

    Add some data augmentation

--- ml.py
+++ ml.py
@@ -55,42 +55,39 @@ def make_model(input_shape, num_classes):
     # ~ x = layers.Conv2D(32, kernel_size=(3, 3), activation="relu")(x)
     # ~ x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     # ~ x = layers.Conv2D(64, kernel_size=(3, 3), activation="relu")(x)
-
     # ~ x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     # ~ x = layers.Flatten()(x)
     # ~ x = layers.Dense(64, activation="relu")(x)
     
     x = layers.Rescaling(1.0 / 255)(inputs)
+    # Augmentation
+    x = layers.RandomFlip()(x)
+    x = layers.RandomRotation(0.2)(x)
+    #x = layers.RandomBrightness(0.2)(x)
+    #x = layers.RandomContrast(0.2)(x)
+    # Convolutions
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
-
     previous_block_activation = x  # Set aside residual
-
     for size in [256, 512, 728]:
         x = layers.Activation("relu")(x)
         x = layers.SeparableConv2D(size, 3, padding="same")(x)
         x = layers.BatchNormalization()(x)
-
         x = layers.Activation("relu")(x)
         x = layers.SeparableConv2D(size, 3, padding="same")(x)
         x = layers.BatchNormalization()(x)
-
         x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
         # Project residual
         residual = layers.Conv2D(size, 1, strides=2, padding="same")(
             previous_block_activation
         )
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
-
     x = layers.SeparableConv2D(1024, 3, padding="same")(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
-
     x = layers.GlobalAveragePooling2D()(x)
-    
     x = layers.Dropout(0.5)(x)
     
     outputs = layers.Dense(len(classes), activation="softmax")(x)
